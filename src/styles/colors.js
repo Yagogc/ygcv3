31a01b640644b7ff1c64a40411db1d0cdc128341
Yago Gonzalez <jagogc@gmail.com>
2018-06-20 00:24:05 +0100

    Add colors

---
+++ src/styles/colors.js
@@ -0,0 +1,16 @@
+const color = {
+  ui1: "#0e1122",
+  ui2: "#151931",
+  ui3: "#1e233b",
+  ui4: "#252c48",
+  ui5: "#ffffff",
+  text1: "#ffffff",
+  text2: "#f0f0f0",
+  text3: "#5a6872",
+  brand1: "#5f4b8b",
+  brand2: "#663399",
+  brand3: "#AA00DE",
+  brand4: "#FF5252"
+};
+
+export default color;

1ff3ce7eea9a9584021dcde3995e2b0fc56c8d1d
Yago Gonzalez <jagogc@gmail.com>
2018-06-20 00:24:17 +0100

    Add global styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import { injectGlobal } from "styled-components";
+import color from "./styles/colors";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
+
+injectGlobal`
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+  }
+  html {
+    background: ${color.ui1};
+    color: ${color.text1}
+  }
+  body {
+    padding: 70px 0;
+  }
+`;

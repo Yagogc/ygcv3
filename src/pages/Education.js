b09bf88503e781ae3fc1d1753d0525ec4470619f
Yago Gonzalez <jagogc@gmail.com>
2018-06-19 22:49:20 +0100

    Add pages Home, Portfolio, Education, Skills, Work

---
+++ src/pages/Education.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Education = () => {
+  return <h1>Education</h1>;
+};
+
+export default Education;
---
+++ src/pages/Work.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Work = () => {
+  return <h1>Work</h1>;
+};
+
+export default Work;

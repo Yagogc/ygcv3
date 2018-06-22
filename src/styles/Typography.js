a2c8badcd849ab8bf6626478f1c445fe7731fe1f
Yago Gonzalez <jagogc@gmail.com>
2018-06-22 16:03:02 +0100

    Updated Typography config

--- src/styles/Typography.js
+++ src/styles/Typography.js
@@ -2,9 +2,15 @@ import WebFont from "webfontloader";
 
 const Typography = () => {
   WebFont.load({
+    classes: false,
+    events: false,
     google: {
       families: ["Open Sans:400,400i,700", "sans-serif"]
-    },
+    }
+  });
+  WebFont.load({
+    classes: false,
+    events: false,
     google: {
       families: ["Trade Winds"],
       text: "aeglnoyz"

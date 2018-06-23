999427fedf1b0f5fe8b10888c3273d8ae835faad
Yago Gonzalez <jagogc@gmail.com>
2018-06-23 19:00:58 +0100

    Add ThemeProvider and Container to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "emotion-theming";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Skills from "./pages/Skills";
@@ -7,23 +8,29 @@ import Work from "./pages/Work";
 import Education from "./pages/Education";
 import Portfolio from "./pages/Portfolio";
 import Error404 from "./pages/404";
+import theme from "./styles/theme";
+import Container from "./styles/Container";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <React.Fragment>
-          <Header />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/skills" component={Skills} />
-            <Route path="/work" component={Work} />
-            <Route path="/education" component={Education} />
-            <Route path="/portfolio" component={Portfolio} />
-            <Route component={Error404} />
-          </Switch>
-        </React.Fragment>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <React.Fragment>
+            <Header />
+            <Container>
+              <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/skills" component={Skills} />
+                <Route path="/work" component={Work} />
+                <Route path="/education" component={Education} />
+                <Route path="/portfolio" component={Portfolio} />
+                <Route component={Error404} />
+              </Switch>
+            </Container>
+          </React.Fragment>
+        </Router>
+      </ThemeProvider>
     );
   }
 }

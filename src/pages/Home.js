b456348a39746a8c6259cbc126958604e449c3f8
Yago Gonzalez <jagogc@gmail.com>
2018-06-23 20:11:55 +0100

    Add Introduction to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Card from "./../styles/Card";
+import styled from "react-emotion";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <Card padded>
+      <Introduction>
+        My name is <Name>Yago Gonzalez</Name>, I’m a Front End Developer
+        specialized in HTML, CSS and JavaScript. I'm passionate about UX design,
+        responsive design and the latest web technologies.
+      </Introduction>
+    </Card>
+  );
 };
 
 export default Home;
+
+const Name = styled.span`
+  font-family: "Marck Script";
+  font-size: 26px;
+`;
+const Introduction = styled.span`
+  line-height: 24px;
+  text-align: justify;
+  display: inline-block;
+`;

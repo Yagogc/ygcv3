e6fbf1a3317c4997a16cca6982521a59b11bc8f9
Yago Gonzalez <jagogc@gmail.com>
2018-06-23 19:00:03 +0100

    Add Theme file

---
+++ src/styles/theme.js
@@ -0,0 +1,24 @@
+const theme = {
+  borderRadius: "50%",
+  borderColor: "#BF67AD",
+  color: {
+    ui1: "#0e1122",
+    ui2: "#151931",
+    ui3: "#1e233b",
+    ui4: "#252c48",
+    ui5: "#ffffff",
+    text1: "#ffffff",
+    text2: "#f0f0f0",
+    text3: "#5a6872",
+    brand1: "#5f4b8b",
+    brand2: "#663399",
+    brand3: "#AA00DE",
+    brand4: "#FF5252"
+  },
+  container: {
+    width: "1200px",
+    padding: "10px"
+  }
+};
+
+export default theme;

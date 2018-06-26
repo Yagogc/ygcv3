20913746862ab3f718e51b42687136bf430a7b6b
Yago Gonzalez <jagogc@gmail.com>
2018-06-26 20:06:16 +0100

    Add Global Styles file

--- src/index.js
+++ src/index.js
@@ -2,29 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { injectGlobal } from "react-emotion";
-import color from "./styles/colors";
 import Typography from "./styles/Typography";
 import Iconography from "./styles/Iconography";
+import GlobalStyles from "./styles/Global";
 
 Typography();
 Iconography();
+GlobalStyles();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
-
-injectGlobal`
-	* {
-		margin: 0;
-		padding: 0;
-    box-sizing: border-box;
-    font-family: "Open sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
-  }
-  html {
-    background: ${color.ui1};
-    color: ${color.text1}
-  }
-  body {
-    padding: 60px 0;
-  }
-`;
---
+++ src/styles/Global.js
@@ -0,0 +1,23 @@
+import { injectGlobal } from "emotion";
+
+import theme from "./theme";
+
+const GlobalStyles = () => {
+  injectGlobal`
+	* {
+		margin: 0;
+		padding: 0;
+    box-sizing: border-box;
+    font-family: "Open sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+  }
+  html {
+    background: ${theme.color.ui1};
+    color: ${theme.color.text1}
+  }
+  body {
+    padding: 60px 0;
+  }
+`;
+};
+
+export default GlobalStyles;

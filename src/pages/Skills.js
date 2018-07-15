a43abb5f64f121f2c87922d7f7824c9ddd897571
Yago Gonzalez <jagogc@gmail.com>
2018-07-15 23:16:12 +0100

    Update Skills page with data

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { List } from "../styles/List";
+
+import skills from "../data/skills";
+// import Container from "./../styles/Container";
+import styled from "react-emotion";
 
 const Skills = () => {
-  return <h1>Skills</h1>;
+  return (
+    <Container>
+      {console.log(skills.length)}
+      {skills.map((skill, i) => (
+        <List
+          name={skill.name}
+          icon={skill.icon}
+          items={skill.skills}
+          iconType={skill.iconType}
+          key={i}
+        />
+      ))}
+    </Container>
+  );
 };
 
 export default Skills;
+
+const Container = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  grid-row: auto;
+
+  @media (${props => props.theme.mq.desktop}) {
+    grid-template-columns: 50% 50%;
+  }
+`;

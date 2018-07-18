4efa2538f6081d2725f71d14739ccdd14be0089b
Yago Gonzalez <jagogc@gmail.com>
2018-07-18 16:05:42 +0100

    Add Works component

---
+++ src/components/Works.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Wrapper, Block } from "../styles/Timeline";
+
+import works from "../data/works";
+
+const Works = () => {
+  return (
+    <Wrapper>
+      {works.map((work, i) => {
+        return (
+          <Block.Wrapper work={work} key={i}>
+            <Block.Disk present={work.isCurrent} />
+            <Block.Card>
+              <Block.Link href={work.companyUrl}>
+                <Block.LinkIcon icon="globe" mright="true" />
+                <Block.Title>{work.company}</Block.Title>
+              </Block.Link>
+              <Block.Info>
+                <Block.Role>
+                  <Block.RoleIcon icon="dot-circle" mright="true" />
+                  {work.role}
+                </Block.Role>
+                <Block.Date>
+                  <Block.DateIcon icon="calendar-alt" mright="true" />
+                  {work.startDate}{" "}
+                  {work.isCurrent || work.endDate ? (
+                    <Block.DateIcon
+                      icon="long-arrow-right"
+                      mright="true"
+                      mleft="true"
+                    />
+                  ) : (
+                    ""
+                  )}{" "}
+                  {work.isCurrent ? "Present" : work.endDate}
+                </Block.Date>
+              </Block.Info>
+              <Block.TechList>
+                {work.techs.map((tech, i) => (
+                  <Block.TechItem key={i}>{tech}</Block.TechItem>
+                ))}
+              </Block.TechList>
+            </Block.Card>
+          </Block.Wrapper>
+        );
+      })}
+    </Wrapper>
+  );
+};
+
+export default Works;

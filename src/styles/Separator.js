45cbc00204220ff0d57fd1bf646e7e336200da6f
Yago Gonzalez <jagogc@gmail.com>
2018-07-28 01:19:22 +0100

    Update Separator style

--- src/styles/Separator.js
+++ src/styles/Separator.js
@@ -4,6 +4,7 @@ const Separator = styled.div`
   height: 10px;
   opacity: 0.7;
   background: ${props => props.theme.gradient()};
+  background: ${props => props.theme.color.ui2};
 `;
 
 export default Separator;

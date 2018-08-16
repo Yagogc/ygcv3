822e2a036e65a8d7f2402750ac6f6ccf94e297ec
Yago Gonzalez <jagogc@gmail.com>
2018-08-16 22:48:51 +0100

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,42 @@
 import React from "react";
+import blackhole from "../assets/blackhole.svg";
+import styled from "react-emotion";
+import Container from "./../styles/Container";
+import { Link as link } from "react-router-dom";
 
 const Error404 = () => {
-  return <h1>Error404</h1>;
+  return (
+    <Container>
+      <Wrapper>
+        <h1>Page not found!</h1>
+        <Img src={blackhole} alt="Blackhole" />
+        <p>But you've found a black hole!</p>
+        <p>
+          <Link to="/">Go back home</Link> before you fall into the event
+          horizon!
+        </p>
+      </Wrapper>
+    </Container>
+  );
 };
-
 export default Error404;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+
+  align-items: center;
+`;
+
+const Img = styled.img`
+  max-width: 300px;
+  width: 100%;
+  padding: 10px 0;
+  pointer-events: none;
+`;
+
+const Link = styled(link)`
+  color: white;
+  font-weight: bold;
+`;
